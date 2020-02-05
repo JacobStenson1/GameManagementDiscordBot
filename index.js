c4d3c265357ebd55baeeb98147e6011a306a54d2
Jacob Stenson <jacob@nosnets.co.uk>
2020-02-05 14:11:00 +0000

    Installed nodemon and added checks
    Installed nodemon.
    Added some console logs for checks when someone messages and when their presence updates.

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const {prefix, token} = require('./config.json');
 
 const bot = new Discord.Client();
 
-bot.on('ready',()=>{
+bot.on('ready', () => {
     console.log("Bot online.")
 });
+
+// When a message is sent to the server.
+bot.on('message', () => {
+    console.log("Message was sent.");
+});
+
+// When a user's presence updates.
+bot.on('presenceUpdate', async(oldMember, newMember) => {
+    if(oldMember.presence.game !== newMember.presence.game){
+        console.log("Presence changed.")
+    }
+});
+
+// Create role and create voice and text channels for it. (May want to seperate this into two seperate functions. One for creating role and another for creating the text and voice stuff.)
+function CreateRoleTextVoiceChannel(){
+
+}
+
 
 bot.login(token);
