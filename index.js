f873a4ae780c1dff8ba2eff7feae78dd6016c3a6
Jacob Stenson <stensonj@coventry.ac.uk>
2020-02-13 14:44:57 +0000

    Working on adding command for adding to the whitelist.

--- index.js
+++ index.js
@@ -58,12 +58,16 @@ bot.on('guildDelete', async(guild) => {
 // When a user's presence updates.
 bot.on('presenceUpdate', async(oldMember, newMember) => {
     if(oldMember.presence.game !== newMember.presence.game){
-        // Return if presence is spotify or if member is a bot or if the presence is now nothing.
+        // Return out if presence is spotify or if member is a bot or if the presence is now nothing.
         //if(newMember.presence.game == "Spotify" || !newMember.bot || newMember.presence == null){ return; }
-        console.log(newMember.displayName+"'s presence changed.");
-        console.log(newMember.guild.name+" - "+newMember.guild.id);
+        console.log(newMember.displayName+"'s in "+ newMember.guild.name+" presence changed.");
+        //console.log(newMember.guild.name+" - "+newMember.guild.id);
 
         var serverWhitelist = require("./ServerWhitelists/"+newMember.guild.id+".json");
+
+        var roleToAdd = newMember.guild.roles.find(x => x.name == roleName);
+
+        //get content from when role is @ by removing <@>
 
         /* // Check to see if the server's whitelist exists
         var ServerWhitelistFilePath = "./ServerWhitelists/"+newMember.guild.id+".json";
@@ -90,8 +94,63 @@ bot.on('presenceUpdate', async(oldMember, newMember) => {
 });
 
 // When a message is sent to the server.
-bot.on('message', message=>{
-    console.log("A message was sent in the server.")
+bot.on('message', async(message) => {
+    console.log("A message was sent in the server.");
+
+    if (message.author.bot){return;}
+
+    let args = message.content.split(" ");
+    //args.shift();
+    
+    /* console.log("here")
+    console.log(args) */
+
+    switch(args[0]){
+        case '!gmtest':
+            message.channel.send("Bot is running!");
+        case '!gmadd':
+            // User only entered 'add' as a command and nothing else.
+            if (args.length == 1){
+                message.reply("Incorrect use of the command. Please use the add command in the form !gm add [Game Name] [Role Name]. Use @RoleName if the role already exists on the server, or no @ if it doesnt exist.");
+                return;
+            }
+            // Use case: !gm ['Game Name'] ['RoleName' OR @roleName]
+
+            // Does user have manage roles permission?
+            if (message.member.hasPermission(['MANAGE_ROLES'])){
+                //console.log("This will add your role.");
+
+                //---------AddRoleToWhitelist(args);
+
+                var roleToAdd;
+                if (message.mentions.roles.first()){
+                    roleToAdd = message.mentions.roles.first().id;
+                    args.pop();
+                }
+                else{
+                    roleToAdd = args.pop();
+                }
+                
+                
+                //console.log(message.mentions.roles.first().name);
+                var gameName = args.slice(1,args.length).join(" ").toString();
+
+                var guild = message.guild;
+                var serverWhitelistFile = "./ServerWhitelists/"+guild.id+".json";
+                var whiteListJson = await require(serverWhitelistFile);
+
+                whiteListJson[gameName] = roleToAdd;
+
+                jsonfile.writeFile(serverWhitelistFile, whiteListJson, { spaces: 2, EOL: '\r\n' }, function(err){
+                    if(err) throw(err);
+                });
+                
+                //console.log(args);
+
+            }else{
+                message.reply("You do not have permission to use that command.");
+            }
+    }
 })
 
 // Functions below ---
