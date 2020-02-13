007f39fcde92f7bcf2becf644e853c931149e445
Jacob Stenson <stensonj@coventry.ac.uk>
2020-02-13 00:29:56 +0000

    More work setting up bot
    Setup messaging system so bot reacts when a message is sent in a server.
    Working on creating save system for json files that store server's whitelists

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const Discord = require('discord.js');
+const jsonfile = require('jsonfile');
 const {prefix, token} = require('./config.json');
 
 const bot = new Discord.Client();
 
+
+// Limits how often saves can happen to avoid abuse
+function scheduleSave(){
+    if(saved){
+        setTimeout(save, minumumSaveInterval);
+        saved = false;
+    }
+}
+
+// Saves the whitelist
+function save(){
+    jsonfile.writeFile("./whitelist.json", whiteListedApps, { spaces: 2, EOL: '\r\n' }, function(err){
+        if (err) throw(err);
+    })
+    console.log("Whitelist file saved.")
+    saved = true;
+}
+
+// Ran when the bot is ready on the server.
 bot.on('ready', () => {
     console.log("Bot online.")
 });
@@ -15,9 +35,26 @@ bot.on('message', () => {
 // When a user's presence updates.
 bot.on('presenceUpdate', async(oldMember, newMember) => {
     if(oldMember.presence.game !== newMember.presence.game){
-        console.log(newMember.displayName+"'s presence changed.")
+        // Return if presence is spotify or if member is a bot or if the presence is now nothing.
+        //if(newMember.presence.game == "Spotify" || !newMember.bot || newMember.presence == null){ return; }
+        console.log(newMember.displayName+"'s presence changed.");
+        console.log(newMember.guild.name+" - "+newMember.guild.id);
+
+        //var serverWhitelist = require("./ServerWhitelists/"+newMember.guild.id+".json")
+
+
+
+        // Is it in the whitelist?
+        // Create the role
+        // Create the categories if the admin says thats ok.
+
+        
     }
 });
+
+bot.on('message', message=>{
+    console.log("A message was sent in the server.")
+})
 
 // Create role and create voice and text channels for it. (May want to seperate this into two seperate functions. One for creating role and another for creating the text and voice stuff.)
 function CreateRoleTextVoiceChannel(){
