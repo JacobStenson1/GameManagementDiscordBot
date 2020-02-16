da4643127f47aa60f565da036fbea1b7dc730da2
Jacob Stenson <stensonj@coventry.ac.uk>
2020-02-16 20:55:01 +0000

    Added settings files for each server. Added to gitignore
    Use server settings when deciding to create categories or not.
    Starting work on the ability to change settings.

--- index.js
+++ index.js
@@ -67,11 +67,13 @@ bot.on('presenceUpdate', async(oldMember, newMember) => {
 
         console.log(`${newMember.displayName}'s presence in ${newMember.guild.name} presence changed.`);
 
-        var serverWhitelist = require("./ServerWhitelists/"+newMember.guild.id+".json");
-        var gameUserIsPlaying = newMember.presence.game.name;
+        let serverWhitelist = require("./ServerWhitelists/"+newMember.guild.id+".json");
+        let gameUserIsPlaying = newMember.presence.game.name;
         roleFromWhitelist = serverWhitelist[gameUserIsPlaying];
-        var roleSearchByID = newMember.guild.roles.find(x => x.id == roleFromWhitelist);
+        let roleSearchByID = newMember.guild.roles.find(x => x.id == roleFromWhitelist);
         var roleToAddToMember;
+
+        let serverSettings = require(`./ServerSettings/${newMember.guild.id}.json`)
         
         console.log(`The game they are now playing is: ${gameUserIsPlaying}`);
         console.log(`Is their game in the server's whitelist? ${gameUserIsPlaying in serverWhitelist}`);
@@ -88,7 +90,7 @@ bot.on('presenceUpdate', async(oldMember, newMember) => {
             if (!roleSearchByName){
                 console.log(`Can't find ${roleFromWhitelist} role. Adding it to server: ${newMember.guild.name}`)
                 // Create the role
-                var roleToAddToMember = await newMember.guild.createRole({name:roleFromWhitelist, mentionable:true});
+                roleToAddToMember = await newMember.guild.createRole({name:roleFromWhitelist, mentionable:true});
                 console.log(`Created ${roleToAddToMember.name} role.`);
             }else{
                 roleToAddToMember = roleSearchByName;
@@ -107,39 +109,38 @@ bot.on('presenceUpdate', async(oldMember, newMember) => {
 
         // if admin has catgegory creation on then create the role category here, based on the game.
 
-        // Only execute this code in the testing server.
-        if(newMember.guild.id == 674598594464186388){
-            // Does the category we are going to create already exist?
-            if(newMember.guild.channels.find(x => x.name == gameUserIsPlaying)){ return; }
+        if (!serverSettings.doCreateCategories){console.log("Dont create categories"); return;}
 
-            // Create category.
-            var newCategory = await newMember.guild.createChannel(gameUserIsPlaying, {type: "category"});
-            // Disallow Everyone to see, join, invite, or speak. Only allow people with the game's role to join.
-            newCategory.overwritePermissions(newMember.guild.defaultRole, {
-                'CREATE_INSTANT_INVITE' : false,        'VIEW_CHANNEL': false,
-                'CONNECT': false,                       'SPEAK': false
-            });
+        // Does the category we are going to create already exist?
+        if(newMember.guild.channels.find(x => x.name == gameUserIsPlaying)){ return; }
 
-            newCategory.overwritePermissions(roleToAddToMember, {
-                'CREATE_INSTANT_INVITE' : false,        'VIEW_CHANNEL': true,
-                'CONNECT': true,                       'SPEAK': true
-            });
-            
+        // Create category.
+        var newCategory = await newMember.guild.createChannel(gameUserIsPlaying, {type: "category"});
+        // Disallow Everyone to see, join, invite, or speak. Only allow people with the game's role to join.
+        newCategory.overwritePermissions(newMember.guild.defaultRole, {
+            'CREATE_INSTANT_INVITE' : false,        'VIEW_CHANNEL': false,
+            'CONNECT': false,                       'SPEAK': false
+        });
 
-            // Create voice channel for game.
-            var newVoiceChannel = await newMember.guild.createChannel(`${gameUserIsPlaying} Voice`, {type: "voice"});
-            await newVoiceChannel.setParent(newCategory);
-            await newVoiceChannel.lockPermissions();
+        newCategory.overwritePermissions(roleToAddToMember, {
+            'CREATE_INSTANT_INVITE' : false,        'VIEW_CHANNEL': true,
+            'CONNECT': true,                       'SPEAK': true
+        });
+        
 
-            // Create text channel for game.
-            var newtTextChannel = await newMember.guild.createChannel(`${gameUserIsPlaying} Text`, {type: "text"});
-            await newtTextChannel.setParent(newCategory);
-            await newtTextChannel.lockPermissions();
+        // Create voice channel for game.
+        var newVoiceChannel = await newMember.guild.createChannel(`${gameUserIsPlaying} Voice`, {type: "voice"});
+        await newVoiceChannel.setParent(newCategory);
+        await newVoiceChannel.lockPermissions();
 
-            console.log("Created category and channel.")
-            
-            console.log(newCategory.name);
-        }       
+        // Create text channel for game.
+        var newtTextChannel = await newMember.guild.createChannel(`${gameUserIsPlaying} Text`, {type: "text"});
+        await newtTextChannel.setParent(newCategory);
+        await newtTextChannel.lockPermissions();
+
+        console.log("Created category and channel.")
+        
+        console.log(newCategory.name);      
     }
 });
 
@@ -148,6 +149,8 @@ bot.on('message', async(message) => {
     if (message.author.bot){return;}
 
     console.log(message.content);
+
+
 
     let args = message.content.split(" ");
     //args.shift();
@@ -183,14 +186,15 @@ bot.on('message', async(message) => {
 
 async function AddRoleToWhitelist(message,args){
     var roleToAdd;
+    var roleName;
     if (message.mentions.roles.first()){
         roleToAdd = message.mentions.roles.first().id;
+        roleName = message.mentions.roles.first();
         args.pop();
     }
     else{
         roleToAdd = args.pop();
     }
-    
     
     //console.log(message.mentions.roles.first().name);
     var gameName = args.slice(1,args.length).join(" ").toString();
@@ -205,20 +209,25 @@ async function AddRoleToWhitelist(message,args){
         if(err) throw(err);
     });
 
-    message.reply(gameName+" added to whitelist with role: "+roleToAdd);
+    message.reply(gameName+" added to whitelist with role: "+roleName);
     console.log("Added game to whitelist.");
 }
 
 function OnJoinSettings(guild){
     let messageContent = `Hello! :smile:
-    Salazhar (the bot creator) thanks you for adding **${botName}** to your server!
-    Use !gmhelp in your server should you need help with the bot.
+    Salazhar (this bot's creator) thanks you for adding **${botName}** to ${guild.name}!
+    Should you need help using the bot type !gmhelp in your server.
     Before the bot is fully working there are a few settings it needs to confirm with you first.
 
     Would you like the bot to create categories and channels for each game it assigns roles for?
     As an example when a user begins playing Battlefield 5 for example, the bot will create a new Category in your server called "Battlefield 5" which will contain both a text and voice channel for Battlefield 5.
     This means that only users with the Battlefield 5 role (people who play Battlefield 5) will see this category.\n
     Would you like this feature in your server? Reply YES or NO.`
+
+    var test = guild.channels;
+    //test.send("test message.");
+
+    console.log(test);
 
     // Send the message to the server owner.
     guild.owner.send(messageContent);
@@ -245,6 +254,13 @@ async function InitialiseNewServer(ServerWhitelistFilePath, guild){
         if(err) throw(err);
     });
 
+    // Setup new server settings
+    var newServerSettingsObj = {"OwnerID": guild.owner.id, "doCreateCategories": false}
+
+    var ServerSettingsPath = "./ServerSettings/"+guild.id+".json";
+    jsonfile.writeFile(ServerSettingsPath, newServerSettingsObj, { spaces: 2, EOL: '\r\n' }, function(err){
+        if(err) throw(err);
+    });
 
     console.log("Added server: "+ guild.name +" to records.");  
 }
